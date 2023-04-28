867158a511e09618bcc378cbbe80d7da45364f61
ch000se <105868069+ch000se@users.noreply.github.com>
2023-04-28 17:02:55 +0300

    update

--- SignalProcessedData.h
+++ SignalProcessedData.h
@@ -10,7 +10,7 @@ public:
     SignalProcessedData();
     SignalProcessedData(double _amplitude, double _frequency, const ProcessedData& _data);
     SignalProcessedData(const SignalProcessedData& other);
-    virtual ~SignalProcessedData();
+    virtual ~SignalProcessedData() override;
 
     void print() const;
 
